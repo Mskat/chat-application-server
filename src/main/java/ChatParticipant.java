2e00ff2939f9a8f36cabe59510b994449fbc7a0e
mskat <zur.katarzyna@gmail.com>
2020-09-14 11:48:52 +0100

    Create ChatParticipant class

---
+++ src/main/java/ChatParticipant.java
@@ -0,0 +1,15 @@
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatParticipant {
+    private List<PrintWriter> chatParticipants = new ArrayList<>();
+
+    public void addParticipant(PrintWriter participant) {
+        chatParticipants.add(participant);
+    }
+
+    public List<PrintWriter> getChatParticipants() {
+        return chatParticipants;
+    }
+}
